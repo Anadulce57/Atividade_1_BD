7436c1b2192fdb3552438c36c22ee052b360748b
Anadulce57 <123557548+Anadulce57@users.noreply.github.com>
2024-04-29 17:19:34 -0300

    Update parte_5.sql

--- parte_5.sql
+++ parte_5.sql
@@ -1,6 +1,18 @@
 -- Questão 4: Produtos Por Faixa de Preço
 use loja;
 
-select nome, preco
+select nome, preco, 1 categoria_preco
 from produtos 
-where preco < 50 and preco >= 50 and preco <=100 and preco > 100
+where preco < 50 
+
+UNION 
+
+select nome, preco, 2 categoria_preco
+from produtos 
+where preco between 50 and 100
+
+UNION
+
+select nome, preco, 3 categoria_preco
+from produtos 
+where preco > 50;
